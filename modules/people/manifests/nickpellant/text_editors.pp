8e3215e4831288d0d6dd81e77374f72b3ed09057
Nick Pellant <nick@nickpellant.com>
2014-07-14 17:47:21 +0100

    Add additional sublime text packages to manifest.

--- modules/people/manifests/nickpellant/text_editors.pp
+++ modules/people/manifests/nickpellant/text_editors.pp
@@ -19,4 +19,28 @@ class people::nickpellant::text_editors {
   sublime_text::package { 'SCSS':
     source => 'MarioRicalde/SCSS.tmbundle'
   }  
+
+  sublime_text::package { 'Handlebars':
+    source => 'daaain/Handlebars'
+  }  
+
+  sublime_text::package { 'SublimePuppet':
+    source => 'russCloak/SublimePuppet'
+  }  
+
+  sublime_text::package { 'Sublime Alignment':
+    source => 'wbond/sublime_alignment'
+  }  
+  
+  sublime_text::package { 'jQuery':
+    source => 'SublimeText/jQuery'
+  }  
+
+  sublime_text::package { 'Trailing Spaces':
+    source => 'SublimeText/TrailingSpaces',
+  }
+
+  sublime_text::package { 'Cucumber':
+    source => 'drewda/cucumber-sublime-bundle',
+  }
 }
