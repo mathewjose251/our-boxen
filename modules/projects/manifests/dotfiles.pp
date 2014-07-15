3b2ffe462310a7dd5e22a57e71e6495df75a317f
Nick Pellant <nick@nickpellant.com>
2014-07-15 15:25:30 +0100

    Link .zshrc from dotfiles.

--- modules/projects/manifests/dotfiles.pp
+++ modules/projects/manifests/dotfiles.pp
@@ -2,4 +2,13 @@ class projects::dotfiles {
   boxen::project { 'dotfiles':
     source => 'nickpellant/dotfiles'
   }
+
+  $home = "/Users/${::boxen_user}"
+  $dotfiles_dir = "${boxen::config::srcdir}/dotfiles"
+
+	file { "${home}/.zshrc":
+    ensure  => link,
+    target  => "${dotfiles_dir}/.zshrc",
+    require => Repository[$dotfiles_dir]
+	}
 }
