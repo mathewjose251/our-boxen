a49ae79ea2b8746d91fac0fca7c0cb1cc3303e0b
Nick Pellant <nick@nickpellant.com>
2014-08-02 13:12:08 +0100

    Add project files for Outposts

---
+++ modules/projects/manifests/outposts-ui.pp
@@ -0,0 +1,5 @@
+class projects::outposts {
+  boxen::project { 'outposts-ui':
+    source => 'nickpellant/outposts-ui'
+  }
+}
---
+++ modules/projects/manifests/outposts.pp
@@ -0,0 +1,7 @@
+class projects::outposts {
+  boxen::project { 'outposts':
+    ruby       => '2.1.2',
+    source     => 'nickpellant/outposts',
+    postgresql => true
+  }
+}
