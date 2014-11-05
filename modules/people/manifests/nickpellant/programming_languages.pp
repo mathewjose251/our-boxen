1e0d492fefb465877a16d7f4cf199069d7a80e97
Nick Pellant <nick@nickpellant.com>
2014-11-05 10:54:10 +0000

    Add fuubar to all rubies in Ruby manifest

--- modules/people/manifests/nickpellant/programming_languages.pp
+++ modules/people/manifests/nickpellant/programming_languages.pp
@@ -1,9 +1,3 @@
 class people::nickpellant::programming_languages {
-  ruby::version { '2.1.4': }
-
-  ruby_gem { 'bundler for all rubies':
-    gem          => 'bundler',
-    version      => '~> 1.0',
-    ruby_version => '*'
-  }
+  include people::nickpellant::programming_languages::ruby
 }
---
+++ modules/people/manifests/nickpellant/programming_languages/ruby.pp
@@ -0,0 +1,15 @@
+class people::nickpellant::programming_languages::ruby {
+  ruby::version { '2.1.4': }
+
+  ruby_gem { 'bundler for all rubies':
+    gem          => 'bundler',
+    version      => '~> 1.0',
+    ruby_version => '*'
+  }
+
+  ruby_gem { 'fuubar for all rubies':
+    gem          => 'fuubar',
+    version      => '~> 2.0',
+    ruby_version => '*'
+  }
+}
