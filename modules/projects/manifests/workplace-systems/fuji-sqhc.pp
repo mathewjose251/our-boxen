6572f50ffe323c0fe591a0379e02f9069dffa24f
Nick Pellant <nick@nickpellant.com>
2014-11-15 10:16:29 +0000

    Namespace the fuji-sqhc project manifest

--- modules/projects/manifests/workplace-systems/fuji-sqhc.pp
+++ modules/projects/manifests/workplace-systems/fuji-sqhc.pp
@@ -1,5 +1,6 @@
-class projects::fuji-sqhc {
+class projects::workplace-systems::fuji-sqhc {
   boxen::project { 'fuji-sqhc':
+    dir    => "${boxen::config::srcdir}/workplace-systems/fuji-sqhc",
     ruby   => '2.1.4',
     source => 'git@gitlab.fuji.ninja:fuji/fuji-sqhc.git',
     mysql  => true
