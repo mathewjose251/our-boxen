e74b549810b1135559cb8dbf09d60611f8c9a9ef
Nick Pellant <nick@nickpellant.com>
2014-11-15 10:14:23 +0000

    Update fuji-sqhc project manifest source

--- modules/projects/manifests/fuji-sqhc.pp
+++ modules/projects/manifests/fuji-sqhc.pp
@@ -1,7 +1,7 @@
 class projects::fuji-sqhc {
   boxen::project { 'fuji-sqhc':
     ruby   => '2.1.4',
-    source => 'git@ec2-54-76-152-102.eu-west-1.compute.amazonaws.com:fuji/fuji-sqhc.git',
+    source => 'git@gitlab.fuji.ninja:fuji/fuji-sqhc.git',
     mysql  => true
   }
 }
