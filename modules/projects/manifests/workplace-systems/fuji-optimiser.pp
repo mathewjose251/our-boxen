741d6633fabadceb8a4509b474a58fe85eed9299
Nick Pellant <nick@nickpellant.com>
2014-11-15 10:20:41 +0000

    Namespace the fuji-optimiser project manifest

--- modules/projects/manifests/workplace-systems/fuji-optimiser.pp
+++ modules/projects/manifests/workplace-systems/fuji-optimiser.pp
@@ -1,5 +1,6 @@
-class projects::fuji-optimiser {
+class projects::workplace-systems::fuji-optimiser {
   boxen::project { 'fuji-optimiser':
+    dir    => "${boxen::config::srcdir}/workplace-systems/fuji-optimiser",
     ruby   => '2.1.4',
     source => 'git@gitlab.fuji.ninja:fuji/fuji-optimiser.git',
     mysql  => true
