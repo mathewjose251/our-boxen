51646dbb3efed2ac1e57fb82b6e0f4bef2bd99a9
Nick Pellant <nick@nickpellant.com>
2014-11-15 10:07:11 +0000

    Add dir to fuji-engage project manifest

--- modules/projects/manifests/workplace-systems/fuji-engage.pp
+++ modules/projects/manifests/workplace-systems/fuji-engage.pp
@@ -1,5 +1,6 @@
 class projects::workplace-systems::fuji-engage {
   boxen::project { 'fuji-engage':
+    dir    => "${boxen::config::srcdir}/workplace-systems/fuji-engage",
     ruby   => '2.1.2',
     source => 'git@gitlab.fuji.ninja:fuji/fuji.git',
     mysql  => true
