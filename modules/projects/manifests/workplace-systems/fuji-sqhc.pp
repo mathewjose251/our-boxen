a114948c80183236a58556d3a2b8f79b2dded7ee
Nick Pellant <nick@nickpellant.com>
2014-11-25 12:43:10 +0000

    Bump fuji-sqhc project to Ruby 2.1.5

--- modules/projects/manifests/workplace-systems/fuji-sqhc.pp
+++ modules/projects/manifests/workplace-systems/fuji-sqhc.pp
@@ -1,7 +1,7 @@
 class projects::workplace-systems::fuji-sqhc {
   boxen::project { 'fuji-sqhc':
     dir    => "${boxen::config::srcdir}/workplace-systems/fuji-sqhc",
-    ruby   => '2.1.4',
+    ruby   => '2.1.5',
     source => 'git@gitlab.fuji.ninja:fuji/fuji-sqhc.git',
     mysql  => true
   }
