0ebd8c07bcc1dafde2c653a2d326b5984d694eff
Nick Pellant <nick@nickpellant.com>
2014-12-28 15:58:12 +0000

    Don't specify ruby version for jarvis-language

--- modules/projects/manifests/nickpellant/jarvis/jarvis-language.pp
+++ modules/projects/manifests/nickpellant/jarvis/jarvis-language.pp
@@ -1,7 +1,6 @@
 class projects::nickpellant::jarvis::jarvis-language {
   boxen::project { 'jarvis-language':
     dir    => "${boxen::config::srcdir}/nickpellant/jarvis/jarvis-language",
-    ruby   => 'jruby-9000-dev',
     source => 'git@gitlab.nickpellant.com:jarvis/jarvis-language.git'
   }
 }
