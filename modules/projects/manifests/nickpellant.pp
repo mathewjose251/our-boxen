13146750d9fef7ba0cf8c571b0f244d14a6b2bc1
Nick Pellant <nick@nickpellant.com>
2014-12-28 16:01:19 +0000

    Remove jarvis-language manifest

--- modules/projects/manifests/nickpellant.pp
+++ modules/projects/manifests/nickpellant.pp
@@ -1,4 +1,3 @@
 class projects::nickpellant {
   include projects::nickpellant::jarvis::jarvis-sensors
-  include projects::nickpellant::jarvis::jarvis-language
 }
--- modules/projects/manifests/nickpellant/jarvis/jarvis-language.pp
+++
@@ -1,6 +0,0 @@
-class projects::nickpellant::jarvis::jarvis-language {
-  boxen::project { 'jarvis-language':
-    dir    => "${boxen::config::srcdir}/nickpellant/jarvis/jarvis-language",
-    source => 'git@gitlab.nickpellant.com:jarvis/jarvis-language.git'
-  }
-}
