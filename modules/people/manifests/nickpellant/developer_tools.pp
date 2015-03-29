a4e5a294b17cb41e0a5bedb6cf4cdfb2fbe4579f
Nick Pellant <nick@nickpellant.com>
2015-03-29 17:41:32 +0100

    Cleanup developer tools manifest

--- modules/people/manifests/nickpellant/developer_tools.pp
+++ modules/people/manifests/nickpellant/developer_tools.pp
@@ -1,19 +1,15 @@
 class people::nickpellant::developer_tools {
   include docker
   include fig
+  include virtualbox
 
-  package { 'iterm2': provider => 'brewcask' }
+  package { 'git-extras': ensure => present }
   package { 'github': provider => 'brewcask' }
+  package { 'iterm2': provider => 'brewcask' }
 
   class { 'phantomjs':
     phantomenv_version => 'v0.0.9',
     phantomenv_repository => 'nickpellant/phantomenv'
   }
   phantomjs::version { '1.9.7': }
-
-  include virtualbox
-
-  package { 'git-extras':
-    ensure => present
-  }
 }
