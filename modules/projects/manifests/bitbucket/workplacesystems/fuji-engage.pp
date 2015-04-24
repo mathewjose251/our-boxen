2636b338db2e9543ccfa282765f3597f4d1a3ecd
Nick Pellant <nick@nickpellant.com>
2015-04-24 11:57:49 +0100

    Update project configuration for fuji-engage

--- modules/projects/manifests/bitbucket/workplacesystems/fuji-engage.pp
+++ modules/projects/manifests/bitbucket/workplacesystems/fuji-engage.pp
@@ -3,6 +3,8 @@ class projects::bitbucket::workplacesystems::fuji-engage {
     dir    => "${boxen::config::srcdir}/bitbucket/workplacesystems/fuji-engage",
     ruby   => '2.1.5',
     source => 'git@bitbucket.org:workplacesystems/fuji-engage.git',
-    mysql  => true
+    mysql  => true,
+    nginx  => true,
+    redis  => true
   }
 }
