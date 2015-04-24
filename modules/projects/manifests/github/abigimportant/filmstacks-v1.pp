5e6b578b8b5ef7f92a6f555f9e0f2965a58bf11c
Nick Pellant <nick@nickpellant.com>
2015-04-24 12:11:19 +0100

    Setup fuji-engage to use nginx

--- modules/projects/manifests/github/abigimportant/filmstacks-v1.pp
+++ modules/projects/manifests/github/abigimportant/filmstacks-v1.pp
@@ -4,7 +4,6 @@ class projects::github::abigimportant::filmstacks-v1 {
   boxen::project { 'filmstacks-v1':
     dir    => "${boxen::config::srcdir}/github/abigimportant/filmstacks-v1",
     mysql  => true,
-    nginx  => 'php/nginx/nginx.conf.erb',
     redis  => true,
     source => 'abigimportant/filmstacks-v1'
   }
