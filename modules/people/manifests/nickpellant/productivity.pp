569275ebc45720ed93772c9f4a6376cc8d0f4429
Nick Pellant <nick@nickpellant.com>
2015-05-18 13:49:45 +0100

    Install CleanMyMac via brew cask

--- modules/people/manifests/nickpellant/productivity.pp
+++ modules/people/manifests/nickpellant/productivity.pp
@@ -2,6 +2,7 @@ class people::nickpellant::productivity {
   package { '1password': provider => 'brewcask' }
   package { 'alfred': provider => 'brewcask' }
   package { 'evernote': provider => 'brewcask' }
+  package { 'cleanmymac': provider => 'brewcask' }
   package { 'dropbox': provider => 'brewcask' }
   package { 'fantastical': provider => 'brewcask' }
   package { 'flux': provider => 'brewcask' }
